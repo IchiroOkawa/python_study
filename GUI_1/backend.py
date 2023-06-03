731a43169df5c16dc63d38c1b749ffc398139157
IchiroOkawa <ichokw@gmail.com>
2023-06-03 15:16:38 +0900

    complete backend

---
+++ GUI_1/backend.py
@@ -0,0 +1,13 @@
+def plus(a,b):
+    """Exmaple function with type documented in the docstring.
+    
+    Args:
+        a (int) : The first parameter.
+        b (int) : The second parameter.
+
+    Returns:
+        c (int) : The return value. c = a + b
+    """
+    c = a + b
+    return c
+
