fb7c68078c7787206596569d7436f7f2c48df902
IchiroOkawa <ichokw@gmail.com>
2023-06-05 23:48:19 +0900

    last commit

--- GUI_3/forntend.py
+++ GUI_3/forntend.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import backend
+import math
 
 input = ''
 memory  = 0
@@ -11,49 +12,72 @@ al_or_not = False
 def click_equal():
     global session,memory,display,cnt,input
     if session == 0:
-        memory = int(input)
-        display.set('0')
+        try:
+            memory = int(input)
+        except:
+            memory += float(input)
     elif session == 1:
-        memory += int(input)
+        try:
+            memory += int(input)
+        except:
+            memory += float(input)
         display.set(str(memory))
         cnt = True
     elif session == 2:
-        memory -= int(input)
+        try:
+            memory -= int(input)
+        except:
+            memory -= float(input)
         display.set(str(memory))
         cnt = True
     elif session == 3:
-        memory *= int(input)
+        try:
+            memory *= int(input)
+        except:
+            memory *= float(input)
         display.set(str(memory))
         cnt = True
     elif session == 4:
-        memory /= int(input)
+        try:
+            memory /= int(input)
+        except:
+            memory /= float(input)
         display.set(str(memory))
         cnt = True
 
 def click_plus():
     global session,memory,display,cnt,input
     if session == 0:
-        memory = int(input)
-        input = ''
-        display.set('0')
+        try:
+            memory = int(input)
+        except:
+            memory += float(input)
     elif session == 1:
-        memory += int(input)
-        input = ''
+        try:
+            memory += int(input)
+        except:
+            memory += float(input)
         display.set(str(memory))
         cnt = True
     elif session == 2:
-        memory -= int(input)
-        input = ''
+        try:
+            memory -= int(input)
+        except:
+            memory -= float(input)
         display.set(str(memory))
         cnt = True
     elif session == 3:
-        memory *= int(input)
-        input = ''
+        try:
+            memory *= int(input)
+        except:
+            memory *= float(input)
         display.set(str(memory))
         cnt = True
     elif session == 4:
-        memory /= int(input)
-        input = ''
+        try:
+            memory /= int(input)
+        except:
+            memory /= float(input)
         display.set(str(memory))
         cnt = True
     
@@ -62,56 +86,74 @@ def click_plus():
 def click_minus():
     global session,memory,display,cnt,input
     if session == 0:
-        memory = int(input)
-        input = ''
-        display.set('0')
+        try:
+            memory = int(input)
+        except:
+            memory += float(input)
     elif session == 1:
-        memory += int(input)
-        input = ''
+        try:
+            memory += int(input)
+        except:
+            memory += float(input)
         display.set(str(memory))
         cnt = True
     elif session == 2:
-        memory -= int(input)
-        input = ''
+        try:
+            memory -= int(input)
+        except:
+            memory -= float(input)
         display.set(str(memory))
         cnt = True
     elif session == 3:
-        memory *= int(input)
-        input = ''
+        try:
+            memory *= int(input)
+        except:
+            memory *= float(input)
         display.set(str(memory))
         cnt = True
     elif session == 4:
-        memory /= int(input)
-        input = ''
+        try:
+            memory /= int(input)
+        except:
+            memory /= float(input)
         display.set(str(memory))
         cnt = True
-    
+
     session = 2
 
 def click_times():
     global session,memory,display,cnt,input
     if session == 0:
-        memory = int(input)
-        input = ''
-        display.set('0')
+        try:
+            memory = int(input)
+        except:
+            memory += float(input)
     elif session == 1:
-        memory += int(input)
-        input = ''
+        try:
+            memory += int(input)
+        except:
+            memory += float(input)
         display.set(str(memory))
         cnt = True
     elif session == 2:
-        memory -= int(input)
-        input = ''
+        try:
+            memory -= int(input)
+        except:
+            memory -= float(input)
         display.set(str(memory))
         cnt = True
     elif session == 3:
-        memory *= int(input)
-        input = ''
+        try:
+            memory *= int(input)
+        except:
+            memory *= float(input)
         display.set(str(memory))
         cnt = True
     elif session == 4:
-        memory /= int(input)
-        input = ''
+        try:
+            memory /= int(input)
+        except:
+            memory /= float(input)
         display.set(str(memory))
         cnt = True
     
@@ -120,113 +162,142 @@ def click_times():
 def click_devision():
     global session,memory,display,cnt,input
     if session == 0:
-        memory = int(input)
-        input = ''
-        display.set('0')
+        try:
+            memory = int(input)
+        except:
+            memory += float(input)
     elif session == 1:
-        memory += int(input)
-        input = ''
+        try:
+            memory += int(input)
+        except:
+            memory += float(input)
         display.set(str(memory))
         cnt = True
     elif session == 2:
-        memory -= int(input)
-        input = ''
+        try:
+            memory -= int(input)
+        except:
+            memory -= float(input)
         display.set(str(memory))
         cnt = True
     elif session == 3:
-        memory *= int(input)
-        input = ''
+        try:
+            memory *= int(input)
+        except:
+            memory *= float(input)
         display.set(str(memory))
         cnt = True
     elif session == 4:
-        memory /= int(input)
-        input = ''
+        try:
+            memory /= int(input)
+        except:
+            memory /= float(input)
         display.set(str(memory))
         cnt = True
     
     session = 4
 
 def click_one():
-    global input,display,cnt
+    global input,display,cnt,al_or_not,c
     if input == '0' or cnt:
         input = '1'
         cnt = False
     else:
-        input += '1'        
+        input += '1'
+    al_or_not = False
+    c.set('C')
     display.set(input)
 
 def click_two():
-    global input,display,cnt
+    global input,display,cnt,al_or_not,c
     if input == '0' or cnt:
         input = '2'
         cnt = False
     else:
         input += '2'
+    al_or_not = False
+    c.set('C')
     display.set(input)
 
 def click_three():
-    global input,display,cnt
+    global input,display,cnt,al_or_not,c
     if input == '0' or cnt:
         input = '3'
     else:
         input += '3'
+    al_or_not = False
+    c.set('C')
     display.set(input)
 
 def click_four():
-    global input,display,cnt
+    global input,display,cnt,al_or_not,c
     if input == '0' or cnt:
         input = '4'
     else:
         input += '4'
+    al_or_not = False
+    c.set('C')
     display.set(input)
 
 def click_five():
-    global input,display,cnt
+    global input,display,cnt,al_or_not,c
     if input == '0' or cnt:
         input = '5'
     else:
         input += '5'
+    al_or_not = False
+    c.set('C')
     display.set(input)
 
 def click_six():
-    global input,display,cnt
+    global input,display,cnt,al_or_not,c
     if input == '0' or cnt:
         input = '6'
     else:
         input += '6'
+    al_or_not = False
+    c.set('C')
     display.set(input)
 
 def click_seven():
-    global input,display,cnt
+    global input,display,cnt,al_or_not,c
     if input == '0' or cnt:
         input = '7'
     else:
         input += '7'
+    al_or_not = False
+    c.set('C')
     display.set(input)
 
 def click_eight():
-    global input,display,cnt
+    global input,display,cnt,al_or_not,c
     if input == '0' or cnt:
         input = '8'
     else:
         input += '8'
+    al_or_not = False
+    c.set('C')
     display.set(input)
 
 def click_nine():
-    global input,display,cnt
+    global input,display,cnt,al_or_not,c
     if input == '0' or cnt:
         input = '9'
     else:
         input += '9'
+    al_or_not = False
+    c.set('C')
     display.set(input)
 
 def click_zero():
-    global input
+    global input,al_or_not,c
     if input == '0' or cnt:
         input = '0'
     else:
         input += '0'
         display.set(input)
+    al_or_not = False
+    c.set('C')
 
 def click_clear():
     global input,al_or_not,memory,session,display,c
@@ -237,10 +308,14 @@ def click_clear():
         cnt = False
         c.set('C')
     else:
-        input = ''
-        display.set('0')
-        al_or_not = True
-        c.set("AC")
+        if memory != 0:
+            input = ''
+            display.set('0')
+            al_or_not = True
+            c.set("AC")
+        else:
+            input = ''
+            display.set('0')
 
 def click_dot():
     global input,display
@@ -250,6 +325,10 @@ def click_dot():
         input += '.'
     display.set(input)
 
+def click_root():
+    global input
+    input = math.sqrt(int(input))
+    display.set(str(input))
 
 # make window
 root = tk.Tk()
@@ -258,13 +337,14 @@ root = tk.Tk()
 root.title('The calculator')
 
 # fit size
-root.geometry('300x200')
+root.geometry('200x200')
 
 # fix size
 # root.resizable(width=False,height=False)
 
 # make frame
-bnt_frame = tk.Frame(root,padx=5,pady=5,relief=tk.RAISED)
+text_frame = tk.Frame(root,padx=10,pady=10)
+bnt_frame = tk.Frame(root,padx=10,pady=10,relief=tk.RAISED)
 
 # make button
 equal_button = tk.Button(
@@ -368,33 +448,40 @@ dot_button = tk.Button(
     text='.',
     command = click_dot
 )
+root_button = tk.Button(
+    bnt_frame,
+    text='√',
+    command=click_root
+)
 
 # label
 display = tk.StringVar(value='0')
-label = tk.Label(textvariable=display)
+label = tk.Label(text_frame,textvariable=display)
 
 # deploy frame
+text_frame.pack(fill=tk.X)
 bnt_frame.pack(fill=tk.X)
 
 # deploy contents
-plus_button.pack()
-minus_button.pack()
-times_button.pack()
-division_button.pack()
-equal_button.pack()
-clear_button.pack()
-dot_button.pack()
-one_button.pack()
-two_button.pack()
-three_button.pack()
-four_button.pack()
-five_button.pack()
-six_button.pack()
-seven_button.pack()
-eight_button.pack()
-nine_button.pack()
-zero_button.pack()
-label.pack() 
+label.pack(side=tk.TOP)
+plus_button.grid(row = 0, column = 4)
+minus_button.grid(row = 1, column = 4)
+times_button.grid(row = 2, column = 4)
+division_button.grid(row = 3, column = 4)
+equal_button.grid(row = 4, column = 4)
+clear_button.grid(row = 0, column = 0)
+dot_button.grid(row= 0,column = 1)
+one_button.grid(row=1,column=0)
+two_button.grid(row=1,column=1)
+three_button.grid(row=1,column=2)
+four_button.grid(row=2,column=0)
+five_button.grid(row=2,column=1)
+six_button.grid(row=2,column=2)
+seven_button.grid(row=3,column=0)
+eight_button.grid(row=3,column=1)
+nine_button.grid(row=3,column=2)
+zero_button.grid(row=4,column=1)
+root_button.grid(row=0,column=2)
 
 # show window
 root.mainloop()
