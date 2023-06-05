4655fada2925dd45360a751c774682a8f1c0e369
IchiroOkawa <ichokw@gmail.com>
2023-06-05 18:34:32 +0900

    GUI_2 complete

--- GUI_2/forntend.py
+++ GUI_2/forntend.py
@@ -12,7 +12,7 @@ def click_plus(a,b):
         input_b.delete(0,tk.END)
         # set answer
         answer.set(backend.plus(a, b))
-    except tk.TclError:
+    except:
         answer.set('Error!')
 
 def click_minus(a,b):
@@ -26,7 +26,7 @@ def click_minus(a,b):
         input_b.delete(0,tk.END)
         # set answer
         answer.set(backend.minus(a, b))
-    except tk.TclError:
+    except:
         answer.set('Error!')
 
 def click_times(a,b):
@@ -40,7 +40,7 @@ def click_times(a,b):
         input_b.delete(0,tk.END)
         # set answer
         answer.set(backend.times(a, b))
-    except tk.TclError:
+    except:
         answer.set('Error!')
 
 def click_devision(a,b):
@@ -54,7 +54,7 @@ def click_devision(a,b):
         input_b.delete(0,tk.END)
         # set answer
         answer.set(backend.devided(a, b))
-    except tk.TclError:
+    except:
         answer.set('Error!')
 
 # make window
@@ -63,36 +63,50 @@ root = tk.Tk()
 # name window
 root.title('The calculator')
 
+# fit size
+root.geometry('300x200')
+
+# fix size
+root.resizable(width=False,height=False)
+
+# make frame
+top_frame = tk.Frame(root,padx=5,pady=5,relief=tk.RAISED)
+bnt_frame = tk.Frame(root,padx=5,pady=5,relief=tk.RAISED)
+
 # a is widget variation
 a = tk.IntVar()
 # make input
-input_a = tk.Entry(textvariable=a)
+input_a = tk.Entry(top_frame,textvariable=a)
 
 # b is widget variation
 b = tk.IntVar()
 # make input
-input_b = tk.Entry(textvariable=b)
+input_b = tk.Entry(top_frame,textvariable=b)
 
 # make button
 plus_button = tk.Button(
+    bnt_frame,
     text= '+',
     cursor='hand2',
     command=lambda: click_plus(a, b)
 )
 minus_button = tk.Button(
+    bnt_frame,
     text= '-',
     cursor='hand2',
     command=lambda: click_minus(a, b)
 )
 times_button = tk.Button(
+    bnt_frame,
     text= '*',
     cursor='hand2',
     command=lambda: click_times(a, b)
 )
 division_button = tk.Button(
+    bnt_frame,
     text= '/',
     cursor='hand2',
-    command=lambda: click_division(a, b)
+    command=lambda: click_devision(a, b)
 )
 
 # set answer as widget 
@@ -100,10 +114,21 @@ answer = tk.StringVar()
 # make label
 label = tk.Label(textvariable=answer)
 
+# message
+message = tk.Label(text='anwer:')
+
+# deploy frame
+top_frame.pack(fill=tk.X)
+bnt_frame.pack(fill=tk.X)
+
 # deploy contents
 input_a.pack()
 input_b.pack()
-plus_button.pack()
+plus_button.pack(side=tk.LEFT,padx=15)
+minus_button.pack(side=tk.LEFT,padx=15)
+times_button.pack(side=tk.LEFT,padx=15)
+division_button.pack(side=tk.LEFT,padx=15)
+message.pack()
 label.pack() 
 
 # show window
